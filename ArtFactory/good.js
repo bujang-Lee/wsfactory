78f3b22a6927f5d5a0b4711f7d8abb393965252c
doseobujang <doseobujang@naver.com>
2021-03-01 13:34:54 +0900

    Update good.js

--- ArtFactory/good.js
+++ ArtFactory/good.js
@@ -1,15 +1,28 @@
+var imgFrameColor = {
+    blue: function(color) {
+        document.querySelector('img').style.borderColor = color;
+    },
+    red: function(color) {
+        document.querySelector('img').style.borderColor = color;
+    },
+    yellow: function(color) {
+        document.querySelector('img').style.borderColor = color;
+    },
+    green: function(color) {
+        document.querySelector('img').style.borderColor = color;
+    }
+}
 function move(self) {
-    var find_img = document.querySelector('img');
     if (self.value == 'blue') {
-        find_img.style.borderColor = 'blue';
+        imgFrameColor.blue('blue');
     }
     else if (self.value == "red") {
-        find_img.style.borderColor = 'red';
+        imgFrameColor.red('red');
     }
     else if (self.value =="yellow") {
-        find_img.style.borderColor = "yellow";
+        imgFrameColor.yellow('yellow');
     }
     else if (self.value =="green") {
-        find_img.style.borderColor = "green";
+        imgFrameColor.green('green');
     }
 }
